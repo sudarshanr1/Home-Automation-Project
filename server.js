70e22389c47c81f7462a56b2a093dbe784608910
Sudarshan Ravindranath <sudarshanravindranath@Sudarshans-MacBook-Pro.local>
2017-02-15 16:04:10 -0800

    small change

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require('express');
 
 var app = express();
-const PORT = process.env.PORT || 3000;
+var PORT = process.env.PORT || 3000;
 app.use(function(request, response, next){
   if(request.headers['x-forwarded-proto'] === 'https') {
     response.redirect('http://' + request.hostname + request.url);

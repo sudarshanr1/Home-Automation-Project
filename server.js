f153ecee0ee733ba02f05aa323116ccaa7ef84e3
Sudarshan Ravindranath <sudarshanravindranath@Sudarshans-MacBook-Pro.local>
2017-02-15 16:32:31 -0800

    server change

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express');
 
 var app = express();
 var PORT = process.env.PORT || 3000;
-app.use(express.static('public/index.html'));
+app.use(express.static('public'));
 app.listen(PORT,function(){
   console.log('Express server '+PORT+' is up');
 })
